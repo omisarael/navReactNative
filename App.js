f0930ebf9459791b29de8adcff6c4c51d16b35c6
omar <paysitemas@gmail.com>
2020-01-23 19:47:55 -0600

    ahora solo stack falta drw switch y tabs

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, Button, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Homevdfd',
-    headerStyle: {
-      backgroundColor: '#ffeecc'
-    },
+    // headerStyle: {
+    //   backgroundColor: '#ffeecc'
+    // },
+    
     headerTintColor: 'green'
+  
   };
   render() {
     const { navigate } = this.props.navigation
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
-        <Button title="a detalle" onPress={() => navigate('Detalle', {paramEnviado: 'param desde home'})}></Button>
+        <Button title="a detalle" onPress={() => navigate('Detalle', { paramEnviado: 'param desde home' })}></Button>
         <Button
           title="Go to Details... again"
           onPress={() => this.props.navigation.push('Detalle')}
@@ -26,18 +28,27 @@ class HomeScreen extends React.Component {
           onPress={() => this.props.navigation.navigate('Detalle')}
         />
         <Button
-        title="go to last screen"
-        onPress={() => this.props.navigation.popToTop()}
-      />
+          title="go to last screen"
+          onPress={() => this.props.navigation.popToTop()}
+        />
       </View>
     );
   }
 }
 
 const DetalleScreen = ({ navigation }) => {
+const [cont, setcont] = useState(0);
+const incrementar = () => {setcont(cont + 1)};
+
+useEffect(() => {
+  navigation.setParams({incrementar})
+  return () => {
+    cont
+  };
+}, [cont])
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Detalle Screen</Text>
+      <Text>Detalle Screen {cont}</Text>
       <Button title="a home" onPress={() => navigation.goBack()}></Button>
       <Button
         title="Go back"
@@ -49,24 +60,36 @@ const DetalleScreen = ({ navigation }) => {
       />
       <Button
         title="SetParams al titulo de navegacion"
-        onPress={() => navigation.setParams({paramEnviado: 'cambiado en detalle con setParamas'})}
+        onPress={() => navigation.setParams({ paramEnviado: 'cambiado en detalle con setParamas' })}
       />
     </View>
   )
 }
 
-DetalleScreen.navigationOptions = ({navigation}) => {
-  return{
-    title: navigation.getParam('paramEnviado',  'parametro defaul Detalle'),
+DetalleScreen.navigationOptions = ({ navigation }) => {
+  return {
+    title: navigation.getParam('paramEnviado', 'parametro defaul Detalle'),
     headerStyle: {
       backgroundColor: '#ffeecc'
     },
     headerTintColor: 'green',
     headerTitleStyle: {
       fontWeight: 'bold'
-    }
+    },
+    headerRight: <Button
+    title="mas 1"
+    onPress={navigation.getParam('incrementar')}
+    color="#8b0000"
+    />
   };
 }
+
+const Logo = () =>
+  <Image
+    style={{ width: 50, height: 50 }}
+    source={require('./assets/icon.png')}
+  />;
+
 
 const AppNavigator = createStackNavigator({
   Home: {
@@ -75,6 +98,19 @@ const AppNavigator = createStackNavigator({
   Detalle: {
     screen: DetalleScreen
   }
-}, { initialRouteName: 'Home' });
+}, {
+  initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: '#d5f518'
+    }
+     ,
+    headerTitle: <Logo />,
+    headerLeft: <Text>fvgbhjk</Text>,
+    // headerRight: <Button
+    // title="ghjkk"
+    // />
+  }
+});
 
 export default createAppContainer(AppNavigator);
